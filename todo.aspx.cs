63a97320778a8a4d0a98b1b2999854c8e7a9ba42
tashib11 <islam2007011@stud.kuet.ac.bd>
2024-03-03 19:43:43 +0600

    todo file  added

---
+++ todo.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace portfolio
+{
+    public partial class todo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addSkillButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/addSkills.aspx");
+        }
+
+        protected void showButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/listSkills.aspx");
+        }
+    }
+}
